c6c3cf6ae5a3dfac8d8bdb0719c84d3a51c0f4c9
anish <anishregmee@gmail.com>
2022-12-19 21:02:50 -0600

    string practice

---
+++ src/day9/StringPractice.java
@@ -0,0 +1,13 @@
+package day9;
+
+public class StringPractice {
+    public static void main(String[] args) {
+        String a1 = "Hello";
+        String a2 = "World";
+
+        System.out.println(a1 == a2);
+        System.out.println(a1.equals(a2));
+
+        System.out.println(a1.toUpperCase());
+    }
+}
