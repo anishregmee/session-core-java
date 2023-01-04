3a061398aba64b0f66b1647ed9e18c1e3d033858
anish <anishregmee@gmail.com>
2023-01-03 19:15:36 -0600

    learning loop

--- src/day10/Marksheet.java
+++ src/day10/Marksheet.java
@@ -6,51 +6,53 @@ public class Marksheet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("enter student\'s name");
-        String studentName = scanner.nextLine();
+        for (int i =0;i<=5;i++){
+            System.out.println("enter student\'s name");
+            String studentName = scanner.next();
 
-        System.out.println("enter marks obtained in maths");
-        float markMaths = scanner.nextFloat();
+            System.out.println("enter marks obtained in maths");
+            float markMaths = scanner.nextFloat();
 
-        System.out.println("enter marks obtained in physics");
-        float markPhysics = scanner.nextFloat();
+            System.out.println("enter marks obtained in physics");
+            float markPhysics = scanner.nextFloat();
 
-        System.out.println("enter marks obtained in computer");
-        float markComputer = scanner.nextFloat();
+            System.out.println("enter marks obtained in computer");
+            float markComputer = scanner.nextFloat();
 
-        System.out.println("enter marks obtained in material");
-        float markMaterial = scanner.nextFloat();
+            System.out.println("enter marks obtained in material");
+            float markMaterial = scanner.nextFloat();
 
-        System.out.println("enter marks obtained in drawing");
-        float markDrawing = scanner.nextFloat();
+            System.out.println("enter marks obtained in drawing");
+            float markDrawing = scanner.nextFloat();
 
-        System.out.println("==================================");
+            System.out.println("==================================");
 
-        float grandTotal = (markMaths + markPhysics + markMaterial + markComputer + markDrawing);
+            float grandTotal = (markMaths + markPhysics + markMaterial + markComputer + markDrawing);
 
-        float percentage = (grandTotal/500) * 100;
+            float percentage = (grandTotal/500) * 100;
 
-        String formattedPercentage = String.format("%.2f", percentage);
+            String formattedPercentage = String.format("%.2f", percentage);
 
-        if((markMaths < 40) || (markPhysics < 40) || (markMaterial < 40) || (markComputer < 40) || (markDrawing < 40)){
-            markDisplay(markMaths,markPhysics,markMaterial,markComputer,markDrawing);
-            System.out.println("sorry, you failed this time,  better luck next time.");
+            if((markMaths < 40) || (markPhysics < 40) || (markMaterial < 40) || (markComputer < 40) || (markDrawing < 40)){
+                markDisplay(markMaths,markPhysics,markMaterial,markComputer,markDrawing);
+                System.out.println("sorry, you failed this time,  better luck next time.");
 
-        } else if (percentage >= 80 ) {
-            markDisplay(markMaths,markPhysics,markMaterial,markComputer,markDrawing);
-            msgDisplay("distinction", studentName, formattedPercentage);
+            } else if (percentage >= 80 ) {
+                markDisplay(markMaths,markPhysics,markMaterial,markComputer,markDrawing);
+                msgDisplay("distinction", studentName, formattedPercentage);
 
-        } else if (percentage >= 65 && percentage <=80) {
-            markDisplay(markMaths,markPhysics,markMaterial,markComputer,markDrawing);
-            msgDisplay("first division", studentName, formattedPercentage);
+            } else if (percentage >= 65 && percentage <=80) {
+                markDisplay(markMaths,markPhysics,markMaterial,markComputer,markDrawing);
+                msgDisplay("first division", studentName, formattedPercentage);
 
-        } else if (percentage >= 55 && percentage < 65) {
-            markDisplay(markMaths,markPhysics,markMaterial,markComputer,markDrawing);
-            msgDisplay("second division", studentName, formattedPercentage);
+            } else if (percentage >= 55 && percentage < 65) {
+                markDisplay(markMaths,markPhysics,markMaterial,markComputer,markDrawing);
+                msgDisplay("second division", studentName, formattedPercentage);
 
-        } else if (percentage >= 40 && percentage < 55) {
-            markDisplay(markMaths,markPhysics,markMaterial,markComputer,markDrawing);
-            msgDisplay("third division", studentName, formattedPercentage);
+            } else if (percentage >= 40 && percentage < 55) {
+                markDisplay(markMaths,markPhysics,markMaterial,markComputer,markDrawing);
+                msgDisplay("third division", studentName, formattedPercentage);
+            }
         }
 
     }
