f3da0b2311ffcb94c2954362b50c72995f226120
anish <anishregmee@gmail.com>
2023-01-09 19:04:05 -0600

    array practice

---
+++ src/day14/ArrayPractice.java
@@ -0,0 +1,44 @@
+package day14;
+
+import java.util.Arrays;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+        int[] arrays1 = {1,2,3,4};
+        int[] arrays2 = {6,7,8,9,};
+
+        //size of array has been declared below
+        String[] names = new String[3];
+
+
+        System.out.println(Arrays.toString(arrays1));
+
+        names[1] = "anish";
+        System.out.println(Arrays.toString(names));
+
+        String value = names[1];
+        System.out.println(value);
+
+        int length = names.length;
+//        for (int i = 0; i < names.length; i++){
+//            String value1 = names[i];
+//            System.out.println(value1);
+//        }
+
+        //enhanced for loop
+        int index = 0;
+        for (String name:names) {
+            System.out.println(name);
+            //index find out garne
+            System.out.println(index);
+            index ++;
+        }
+
+        int[] scope = new int [10];
+        Arrays.fill(scope,22);
+        System.out.println(Arrays.toString(scope));
+
+
+
+    }
+}
---
+++ src/day14/ArrayProperty.java
@@ -0,0 +1,24 @@
+package day14;
+
+import java.util.Arrays;
+
+public class ArrayProperty {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,2,4,3};
+        System.out.println(Arrays.toString(a));
+//        randomFunction(a);
+//        System.out.println(Arrays.toString(a));
+
+        //array function maa pass garda copy of array pathaune.. reason- value pass garna hoina reference pass garna
+//        int[] copyOfA = Arrays.copyOf(a,a.length);
+//        randomFunction(copyOfA);
+//        System.out.println(Arrays.toString(copyOfA));
+
+        //arrange garnalai use hune important function
+        Arrays.sort(a);
+    }
+
+    public static void randomFunction(int[] a){
+        a[0] = 8;
+    }
+}
